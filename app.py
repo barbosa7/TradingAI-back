94dcde16acae9983452ef5d8b00c2877e11d714b
tomasbarbosa <tomasgiaossbarbosa@gmail.com>
2022-11-24 14:30:09 +0100

    more info about current positions

--- app.py
+++ app.py
@@ -5,14 +5,26 @@ app = Flask(__name__)
 CORS(app)
 
 standings = []
+btc_price = 0
+btc_balance = 0
+usdt_balance = 0
 
 @app.route('/',methods = ['POST','GET'])
 def show_info():
     if request.method == 'POST':
         data = request.form['info']
+        btc_price = request.form['btc_price']
+        btc_balance = request.form['btc_balance']
+        usdt_balance = request.form['usdt_balance']
+
         print('we just got some data')
         standings.append(data)
         print(data)
         return standings
     else:
         return standings
+
+@app.route('/',methods = ['POST','GET'])
+def show_info():
+    if request.method == 'GET':
+        return [btc_price,btc_balance,usdt_balance]
